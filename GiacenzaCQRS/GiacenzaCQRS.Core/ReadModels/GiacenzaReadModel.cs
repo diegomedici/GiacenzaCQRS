84541aec4e141386588df0bde32d82e044bda2f5
Diego Medici <diego.medici@studiofarma.it>
2013-05-07 17:23:40 +0200

    ReadModel e Projection

---
+++ GiacenzaCQRS/GiacenzaCQRS.Core/ReadModels/GiacenzaReadModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using GiacenzaCQRS.Core.Events;
+
+namespace GiacenzaCQRS.Core.ReadModels
+{
+    public class GiacenzaReadModel
+    {
+        
+        public Guid Id { get; set; }
+        public string Minsan { get; set; }
+        public int Quantita { get; set; }
+    }
+}
